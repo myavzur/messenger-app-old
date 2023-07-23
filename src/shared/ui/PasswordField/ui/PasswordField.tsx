aa1e9960bb0574f7d227cf78508f8959c9e6fecd
myavzur <shwave_id404@mail.ru>
2023-07-23 22:40:10 +0300

    npm audit fix

---
+++ src/shared/ui/PasswordField/ui/PasswordField.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { PasswordFieldProps } from "./PasswordField.interface";
+
+const PasswordField: React.FC<PasswordFieldProps> = ({  }) => {
+	return (
+		<>
+			PasswordField
+		</>
+	);
+};
+
+export default PasswordField;
